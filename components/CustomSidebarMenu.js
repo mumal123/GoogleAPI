6cd620af53ca71cb56cae918b014c9b58fd4bd5b
Mumal Swami <66372736+mumal123@users.noreply.github.com>
2020-11-12 12:19:18 +0530

    Update CustomSidebarMenu.js

--- components/CustomSidebarMenu.js
+++ components/CustomSidebarMenu.js
@@ -1,24 +1,31 @@
-import React, { Component} from 'react';
-import {View, Text,TouchableOpacity} from 'react-native';
-import { DrawerItems} from 'react-navigation-drawer'
-
-import firebase from 'firebase';
-
-export default class CustomSidebarMenu extends Component{
-  render(){
-    return(
-      <View style={{flex:1}}>
-        <DrawerItems {...this.props}/>
-        <View style={{flex:1,justifyContent:'flex-end',paddingBottom:30}}>
-          <TouchableOpacity style={{justifyContent:'center',padding:10,height:30,width:'100%'}}
-          onPress = {() => {
-              this.props.navigation.navigate('WelcomeScreen')
-              firebase.auth().signOut()
-          }}>
-            <Text>Logout</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-    )
-  }
-}
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  drawerItemsContainer: {
+    flex: 0.8,
+  },
+  logOutContainer: {
+    flex: 0.2,
+    justifyContent: "flex-end",
+    paddingBottom: 30,
+  },
+  logOutButton: {
+    height: 30,
+    width: "100%",
+    justifyContent: "center",
+    padding: 10,
+  },
+  imageContainer: {
+    flex: 0.75,
+    width: "40%",
+    height: "20%",
+    marginLeft: 20,
+    marginTop: 30,
+    borderRadius: 40,
+  },
+  logOutText: {
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+});
